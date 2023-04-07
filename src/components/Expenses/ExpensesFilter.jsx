89f8ec14edbe3525b49e4c0714d27b4c7e391d4e
Pooja R <pooja.ramesh@imaginelearning.com>
2023-04-07 12:29:48 +0530

    Adding Filter

---
+++ src/components/Expenses/ExpensesFilter.jsx
@@ -0,0 +1,23 @@
+import "./Styles/ExpensesFilter.css";
+
+const ExpensesFilter = (props) => {
+  const ExpenseChange = (event) => {
+    props.onYearChangeHandler(event.target.value);
+  };
+
+  return (
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
+        <label>Filter by year</label>
+        <select onChange={ExpenseChange}>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default ExpensesFilter;
